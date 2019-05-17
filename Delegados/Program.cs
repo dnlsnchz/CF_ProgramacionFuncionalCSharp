4801ab8469feb3f45b779484ef077bab328311ff
Daniel Sanchez luyo <dnlsnchz4@gmail.com>
2019-05-17 00:53:30 -0500

    Se suben proyectos al git

---
+++ Delegados/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Delegados
+{
+    class Program
+    {
+        public delegate float DelegateOperacion (float val1, float val2);
+        //Depositos
+        //Retiros
+        public static float Deposito(float cantidad, float monto){
+            return cantidad+monto;
+        }
+        public static float Retiro(float cantidad, float monto){
+            if(cantidad>monto){
+                Console.WriteLine("No es posible realizar el retiro");
+                return 0.0f;
+            }
+            return monto-cantidad;
+        }
+        public static float EjecutarOperacion (DelegateOperacion operacion, float cantidad, float monto){
+            Console.WriteLine("Esta por ejecutar la operación");
+            float result = operacion(cantidad,monto);
+            Console.WriteLine(result);
+            Console.WriteLine("La operación se ha ejecutado");
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            DelegateOperacion retiro=Retiro;
+            DelegateOperacion deposito = Deposito;
+
+            //EjecutarOperacion (retiro,10,100);
+
+            //Delegados con expresiones lamda
+            DelegateOperacion depositoInteres=(cantidad,monto)=>
+            {
+                if(cantidad>100){
+                    return cantidad + monto +(cantidad*0.02f);
+                }
+                return cantidad+monto;
+            };
+
+            EjecutarOperacion(depositoInteres,101,100);
+        }
+    }
+}
---
+++ funtional/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace funtional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var calificaciones = new List<int>{10,10,5,7,7,10,8,9,9, 10, 8};
+            ////obtener calificaciones mayores a 8
+
+            ////Imperativo
+            //foreach(var calificacion in calificaciones){
+            //    if (calificacion>8){
+            //        Console.WriteLine(calificacion);
+            //    }
+            //}
+            //Console.WriteLine("");
+            //Declarativo
+            //var result = calificaciones.Where(calificacion => calificacion>8);
+            //foreach(var calificacion in result)
+            //    Console.WriteLine(calificacion);
+            
+            
+            EncontrarElemento();
+        }
+        public static void EncontrarElemento(){
+            var numeros = new List<int>{1,2,3,4,5,6,7,8,9,10};
+            Console.WriteLine(":D");
+            int numeroBuscar=50;
+            bool existeConstaint = numeros.Contains(numeroBuscar);
+            Console.WriteLine(existeConstaint);
+            
+            bool existeAny = numeros.Any(numero=> numero== numeroBuscar);
+            Console.WriteLine(existeConstaint);
+            
+            int resultado = numeros.Find(numero=> numero== numeroBuscar);
+            Console.WriteLine(resultado);
+            
+        }
+        public static void ReduccionElemento(){
+            var numeros = new List<int>{1,2,3,4,5,6,7,8,9,10};
+
+            //Imperativo
+            int suma=0;
+            foreach(var numero in numeros)
+                suma+=numero;
+            Console.WriteLine(suma);
+            Console.WriteLine("");
+
+            //Declarativo
+            int resultado = numeros.Aggregate( (acumulador, numero)=> acumulador+numero);
+            Console.WriteLine(resultado);
+        }
+    }
+}
